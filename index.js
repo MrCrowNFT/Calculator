7187918bf8a77e6e44d80076ba64163a96a4d4eb
MrCrowNFT <mrcrownft@gmail.com>
2024-12-06 08:55:25 -0300

    Complete app

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 //Calculator
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input
+}
+
+function calculate(){
+    //eval basically calculates an expression
+    //this can cause an error
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
+
+function clearDisplay(){
+    display.value = ""
+}
